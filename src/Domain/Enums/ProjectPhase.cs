8aa5acc0e66f05b122b179eec128d4cdbb1ae2ec
Anton <anton.schuh@aschuh.de>
2025-01-26 13:52:17 +0100

    use string enums

--- src/Domain/Enums/ProjectPhase.cs
+++ src/Domain/Enums/ProjectPhase.cs
@@ -1,5 +1,8 @@
-﻿namespace Domain.Enums;
+﻿using System.Text.Json.Serialization;
 
+namespace Domain.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum ProjectPhase
 {
     Idea, Request, Planning, Ongoing, Finished, Cancelled, Unkown
--- src/Domain/Enums/ProjectType.cs
+++ src/Domain/Enums/ProjectType.cs
@@ -1,5 +1,8 @@
-﻿namespace Domain.Enums;
+﻿using System.Text.Json.Serialization;
 
+namespace Domain.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum ProjectType
 {
     Idea, Inspiration, Project, ProjectRequest, Unkown
