a61e3c81d76c27ec37f4798fae4703e013c4b129
Anton Schuh <anton.schuh@aschuh.de>
2025-05-08 16:25:35 +0200

    use csr for user area

--- src/Frontend/src/app/app.config.server.ts
+++ src/Frontend/src/app/app.config.server.ts
@@ -2,11 +2,15 @@ import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 import { provideServerRouting, RenderMode } from '@angular/ssr';
+import {AppRouteNames} from "./app.routes.names";
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
     provideServerRouting([{
+      path: `${AppRouteNames.RestrictedName}/**`,
+      renderMode: RenderMode.Client
+    }, {
       path: '**',
       renderMode: RenderMode.Server
     }]),
