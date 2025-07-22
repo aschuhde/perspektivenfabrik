219ba8315f04a6555562ace95a5304689f882ddf
Anton Schuh <anton.schuh@aschuh.de>
2025-07-22 18:37:44 +0200

    - renamed to Impressum - cookie policy

--- src/Frontend/src/app/features/home/home-route-names.ts
+++ src/Frontend/src/app/features/home/home-route-names.ts
@@ -6,6 +6,7 @@ export class HomeRouteNames{
   public static readonly ProjectsName = "projects";
   public static readonly AboutUsName = "about-us";
   public static readonly PrivacyPolicyName = "privacy";
+  public static readonly CookiesName = "cookies";
   public static readonly LegalNoticeName = "legal-notice";
   public static readonly ProjectName = `${HomeRouteNames.ProjectsName}/:projectIdentifier`;
   public static readonly InternalName = "internal";
@@ -14,6 +15,7 @@ export class HomeRouteNames{
   public static readonly AboutUsUrl = () => `${AppRouteNames.HomeUrl()}/${HomeRouteNames.AboutUsName}`;
   public static readonly LegalNoticeUrl = () => `${AppRouteNames.HomeUrl()}/${HomeRouteNames.LegalNoticeName}`;
   public static readonly PrivacyPolicyUrl = () => `${AppRouteNames.HomeUrl()}/${HomeRouteNames.PrivacyPolicyName}`;
+  public static readonly CookiesUrl = () => `${AppRouteNames.HomeUrl()}/${HomeRouteNames.CookiesName}`;
 
   public static ProjectUrl = (entityId: string) => `${AppRouteNames.HomeUrl()}/${HomeRouteNames.ProjectsName}/${entityId}`;
   public static InternalProjectUrl = (entityId: string) => `${AppRouteNames.HomeUrl()}/${HomeRouteNames.InternalName}/${HomeRouteNames.ProjectsName}/${entityId}`;
