798941dd21705e8b31d2a006b1352ff217110c23
Anton Schuh <anton.schuh@aschuh.de>
2025-09-24 18:42:47 +0200

    translations

--- src/Frontend/src/app/shared/components/speech-bubble/speech-bubble.component.ts
+++ src/Frontend/src/app/shared/components/speech-bubble/speech-bubble.component.ts
@@ -1,4 +1,5 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-speech-bubble',
@@ -7,5 +8,8 @@ import { Component, input } from '@angular/core';
   styleUrl: './speech-bubble.component.scss'
 })
 export class SpeechBubbleComponent {
-
+  translateService = inject(TranslateService);
+  get language(){
+    return this.translateService.currentLang;
+  }
 }
