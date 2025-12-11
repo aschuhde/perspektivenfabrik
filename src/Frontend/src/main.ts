c281f81571c78829451121ae0eedbc656145a903
Anton Schuh <anton.schuh@aschuh.de>
2025-12-11 13:45:57 +0100

    updated angular cli

--- src/Frontend/src/main.ts
+++ src/Frontend/src/main.ts
@@ -1,8 +1,9 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { register as registerSwiperElements  } from 'swiper/element/bundle';
 
 registerSwiperElements();
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]})
   .catch((err) => console.error(err));
