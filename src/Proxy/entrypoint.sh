7ddb73712dd4980c2d3ee30b76edead2582a792d
Anton Schuh <anton.schuh@aschuh.de>
2026-02-09 12:03:12 +0100

    removed cert and https configuration in dockerfile and entrypoint.sh

--- src/Proxy/entrypoint.sh
+++ src/Proxy/entrypoint.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-mkdir -p /var/ssl
-if [ -n "$PROXY_CERTIFICATE" ]; then echo "$PROXY_CERTIFICATE" > cert.txt; base64 -d cert.txt > /var/ssl/certificate.user.pfx; fi
 cd /app & dotnet Proxy.dll
